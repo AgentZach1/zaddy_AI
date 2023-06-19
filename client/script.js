a52a8b4739c0b301a4076bb124e778c2890b0999
AgentZach2 <58308826+AgentZach2@users.noreply.github.com>
2023-06-19 18:09:14 -0400

    fixed endpoint

--- client/script.js
+++ client/script.js
@@ -81,9 +81,9 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  // get data from server
+  // get data from server hosted from... :https://zaddy-ai.onrender.com
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://zaddy-ai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
