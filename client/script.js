78f1dafe384ee6a51fa7c3a6f185b8d91793fb5e
AgentZach2 <58308826+AgentZach2@users.noreply.github.com>
2023-06-20 08:54:08 -0400

    the bot can remember now and I fixed the fetch

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   // get data from server hosted from... :https://zaddy-ai.onrender.com
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://zaddy-ai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
